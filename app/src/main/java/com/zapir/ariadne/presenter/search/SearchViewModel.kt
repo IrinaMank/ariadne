28272be3dbd23973cd0393117c87d1dc3f0bd6f1
irina_mank <Irina.Mankovskaya@softomate.com>
2018-11-26 16:33:45 +0700

    add progress

--- app/src/main/java/com/zapir/ariadne/presenter/search/SearchViewModel.kt
+++ app/src/main/java/com/zapir/ariadne/presenter/search/SearchViewModel.kt
@@ -2,6 +2,7 @@ package com.zapir.ariadne.presenter.search
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
 import com.zapir.ariadne.model.entity.Waypoint
 import com.zapir.ariadne.model.repositories.PointsRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -13,18 +14,29 @@ class SearchViewModel(
 
     var from = MutableLiveData<Waypoint>()
     var to = MutableLiveData<Waypoint>()
+    var state = MutableLiveData<WaypointsState>()
     var points = MutableLiveData<List<Waypoint>>()
 
     fun getPoints() =
             interactor.getPoints()
+                    .doOnSubscribe {
+                        Log.e("dlf", "BEGIN")
+                        state.postValue(WaypointsState.LoadingState(true))
+                    }
+                    .doFinally {
+                        state.postValue(WaypointsState.LoadingState(false))
+                        Log.e("dlf", "TERMINATE")
+                    }
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
                             {
-                                points.postValue(it)
+                                state.postValue(WaypointsState.SuccessState(it))
+                                //points.postValue(it)
                             },
                             {
                                 print(it.message)
+                                state.postValue(WaypointsState.FailState(it))
 
                             }
                     )
