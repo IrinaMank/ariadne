5f32fe4b916dca687a785a55c8ebba04f6dbd568
IrinaMank <zapirinamank@gmail.com>
2018-12-02 22:23:27 +0700

    filter hall and ladder

--- app/src/main/java/com/zapir/ariadne/model/entity/common/PointType.kt
+++ app/src/main/java/com/zapir/ariadne/model/entity/common/PointType.kt
@@ -1,5 +1,5 @@
 package com.zapir.ariadne.model.entity.common
 
 enum class PointType(val value: String) {
-    ROOM("Room"), LADDER("Ladder"), ENTRY("Entry"), HALL("Hall")
+    ROOM("Room"), LADDER("Ladder"), ENTRY("Entry"), HALL("Corridor")
 }
